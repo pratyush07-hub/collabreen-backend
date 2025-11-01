c2ddb341122498b9d5408dae8b140ce70feaa552
Pratyush <choudharypratyush809@gmail.com>
2025-11-01 19:28:52 +0530

    bugs

--- socket/groupChat.js
+++ socket/groupChat.js
@@ -1,4 +1,4 @@
-const Group = require('../models/Group');
+const Group = require('../models/group');
 const GroupMessage = require('../models/groupMessage');
 
 module.exports = (io, socket, connectedUsers) => {
