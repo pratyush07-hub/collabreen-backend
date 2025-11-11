0a73d872d5378acaa988f26b4ec7d213fffe67db
Pratyush <choudharypratyush809@gmail.com>
2025-11-12 01:13:50 +0530

    db changes

--- config/connectMongo.js
+++ config/connectMongo.js
@@ -1,9 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+let isConnected = false;
 
 async function connectMongo(url) {
-    await mongoose.connect(url)
-    .then(()=>console.log("Mongo Connected"))
-    .catch(err => console.error("Mongo connection error:", err));
+  if (isConnected) {
+    console.log("MongoDB already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 }
 
-module.exports = connectMongo
+module.exports = connectMongo;
